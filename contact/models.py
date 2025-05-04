b13c396157d69ab31257200c4d491aa66f895770
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-05-04 18:44:31 -0300

    configurando chave estrangeira category

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return f'{self.name}'
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -10,6 +15,7 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%M/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
