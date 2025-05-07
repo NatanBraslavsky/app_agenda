dfaad16c7c42ad2182a6fbe8f3f9215740c09d9d
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-05-06 23:24:32 -0300

    meta options criada

--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,13 @@
+from tabnanny import verbose
 from django.db import models
 from django.utils import timezone
 
 #criar uma entidade
 class Category(models.Model):
     #cria um atributo
+    class Meta:
+        verbose_name = 'Category' 
+        verbose_name_plural = 'Categories'
     name = models.CharField(max_length=50)
     def __str__(self):
         return f'{self.name}'
