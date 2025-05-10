f0a5915fb614caca69f4a4c9cd0219880dedba34
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-05-10 15:52:43 -0300

    criando area de contatos unicos ao clicar no id para informações unicas do contato

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,6 @@ from contact import views
 app_name = 'contact'#definir a chamada do app
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),#definir o caminho
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from contact.models import Contact
 
+
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).all().order_by('-id')[0:10]
     context = {
         'contacts' : contacts,
     }
     return render(request, 'contact/index.html',context)#direcionar a url do html
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact, pk=contact_id)
+    
+    context = {
+        'contact' : single_contact,
+    }
+    return render(request, 'contact/contact.html',context)#direcionar a url do html
 
