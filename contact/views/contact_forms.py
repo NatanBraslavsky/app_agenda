788c58530fdb855b089ead5bb62e32374266442e
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-05-21 13:18:03 -0300

    configurando os campos e widgets do formulário

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,23 +1,8 @@
-from dataclasses import field
-from typing import Any, Dict
+
 from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q
-from django.core.paginator import Paginator
-from contact.models import Contact
-from django import forms
-from django.core.exceptions import ValidationError
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('first_name','last_name', 'phone',)
+from contact.forms import ContactForm
 
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error('first_name',ValidationError('Mensagem de erro', code='invalid'))
-
-        return super().clean()
     
 def create(request):
     
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -25,7 +25,6 @@ def search(request):
     
     contacts = Contact.objects.filter(show=True).filter(Q(first_name__icontains=search_value) | 
     Q(last_name__icontains=search_value) | Q(phone__icontains=search_value) | Q(email__icontains=search_value)).order_by('-id')
-
     paginator = Paginator(contacts, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
