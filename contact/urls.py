593ad1a6175c2a045b919da2c75d79dfdbaa43fc
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-05-22 14:03:32 -0300

    atualizar dados de um contato

--- contact/urls.py
+++ contact/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
 ]
