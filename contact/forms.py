8ccc30484072d0e97e46f4c38101902e1d0e366b
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-05-24 14:57:39 -0300

    image field adicionado no forms

--- contact/forms.py
+++ contact/forms.py
@@ -4,33 +4,10 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
-
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class': 'classe-a classe-b',
-            }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário',
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a classe-b',
-        #     'placeholder': 'Aqui veio do init',
-        # })
-
+    picture = forms.ImageField(widget=forms.FileInput(attrs={'accept':'image/*',}))
     class Meta:
         model = models.Contact
-        fields = ('first_name','last_name', 'phone','email', 'description', 'category',)
-        # widgets = {
-        #     'first_name': forms.TextInput(attrs={
-        #         'class':'class-a class-b',
-        #     })
-        # }
+        fields = ('first_name','last_name', 'phone','email', 'description', 'category', 'picture',)
 
     def clean(self):
         cleaned_data = self.cleaned_data
